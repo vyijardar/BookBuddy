c54870e9ac8a9566bbb37faa5ddaf5a8bffbc148
feature-A <vyijardar@YuVi>
2024-11-26 22:29:10 -0600

    Updated code

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom"
 
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+
   </React.StrictMode>,
 )
